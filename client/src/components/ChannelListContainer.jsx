7ea376a04640ad6f127faaee5f9d9c1cc22a8d83
SSLidder <81623102+SSLidder@users.noreply.github.com>
2023-02-09 15:57:35 +0000

    Structure Setup Complete
    Initial structure setup complete, sidebar and channels setup

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,10 +1,72 @@
 import React from "react";
+import { Channel, ChannelList, useChatContext } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import BooksIcon from '../assets/books.jpeg';
+import LogoutIcon from '../assets/logout.png';
+
+const SideBar = () => (
+    <div className="channel-sidebar">
+        <div className="channel-sidebar-icon">
+            <div className="inner-icon">
+                <img src={BooksIcon} alt="Books" width="30" />
+            </div>
+        </div>
+        <div className="channel-sidebar-icon1">
+            <div className="inner-icon">
+                <img src={LogoutIcon} alt="Logout" width="30" />
+            </div>
+        </div>
+    </div>
+);
+
+const CompanyHeader = () => (
+    <div className="channel-header">
+        <p className="channel-header-text">University Page</p>
+    </div>
+)
 
 const ChannelListContainer = () => {
     return (
-        <div>
-            Channel List Container
-        </div>
+        <>
+            <SideBar />
+            <div className="channel-wrapper">
+                <CompanyHeader/>
+                <ChannelSearch/>
+                <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => {}}
+                    List={(listProps) => (
+                        <TeamChannelList
+                            {...listProps}
+                            type="team"
+                        />
+                    )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview
+                            {...previewProps}
+                            type="team"
+                        />
+                    )}
+                />
+                <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => {}}
+                    List={(listProps) => (
+                        <TeamChannelList
+                            {...listProps}
+                            type="messaging"
+                        />
+                    )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview
+                            {...previewProps}
+                            type="messaging"
+                        />
+                    )}
+                />
+            </div>
+        </>
     );
 }
 
