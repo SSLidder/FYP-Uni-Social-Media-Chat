8c12f3665114fd3beaa8146625c54670f5042ca4
SSLidder <81623102+SSLidder@users.noreply.github.com>
2023-02-13 16:36:32 +0000

    Sign up and Login form created
    The client side form has been created and the console outputs the form values on submit

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -3,19 +3,37 @@ import Cookies from "universal-cookie";
 import axios from 'axios';
 import signUpBG from '../assets/signupbg.png';
 
+const initialState = {
+    fullName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    phoneNumber: '',
+    email: '',
+}
+
 const Auth = () => {
     const [signUp, setSignUp] = useState(true);
-    const handleChange = () => {};
+    const handleChange = (e) => {
+        setForm({...form, [e.target.name]: e.target.value}); 
+    
+        // console.log(form);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(form);
+    };
     const switchForm = () => {
         setSignUp((prevSignUp) => !prevSignUp);
     }
+    const [form, setForm] = useState();
 
     return(
         <div className="auth-container">
             <div className="auth-fields">
                 <div className="auth-fields-content">
                     <p>{signUp ? 'Sign Up' : 'Login'}</p>
-                    <form onSubmit={() => {}}>
+                    <form onSubmit={handleSubmit}>
                         {signUp && (
                             <div className="auth-fields-input">
                                 <label htmlFor='fullName'>Full Name</label>
@@ -40,10 +58,22 @@ const Auth = () => {
                             </div>
                         {signUp && (
                             <div className="auth-fields-input">
+                                <label htmlFor='email'>Email</label>
+                                <input
+                                    name="email"
+                                    type="text"
+                                    placeholder="Email Address"
+                                    onChange={handleChange}
+                                    required 
+                                />
+                            </div>
+                        )}
+                        {signUp && (
+                            <div className="auth-fields-input">
                                 <label htmlFor='phoneNumber'>Phone Number</label>
                                 <input
                                     name="phoneNumber"
-                                    type="text"
+                                    type="number"
                                     placeholder="Phone Number"
                                     onChange={handleChange}
                                     required 
@@ -72,6 +102,9 @@ const Auth = () => {
                                 />
                             </div>
                         )}
+                        <div className="auth-form-button">
+                            <button>{signUp ? "Sign Up" : "Sign In"}</button>
+                        </div>
                     </form>
                     <div className="auth-account">
                         <p>   
@@ -82,6 +115,9 @@ const Auth = () => {
                         </p>
                     </div>
                 </div>
+            </div>
+            <div className="auth_form-container-image">
+                <img src={signUpBG} alt="Sign In" />
             </div>
         </div> 
     )
